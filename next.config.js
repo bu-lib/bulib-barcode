8c8f5ad7ce05d821e4c0f866fda51205eb1d9186
unknown <a.v.musa2017@gmail.com>
2022-09-12 13:53:09 +0800

    changed build configs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    images: {
+      unoptimized: true
+    }
+  },
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId}) {
+    return {
+      "/": {page: "/"}
+    }
+  },
+  assetPrefix: './'
 }
 
 module.exports = nextConfig
